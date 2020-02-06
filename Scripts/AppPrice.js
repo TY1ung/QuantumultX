33ca84333140cc678c93453ab8bd4419c3580d9e
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2020-02-07 07:01:25 +0800

    Update AppPrice.js

--- Scripts/AppPrice.js
+++ Scripts/AppPrice.js
@@ -145,7 +145,7 @@ if (isSurge) {
 // #endregion
 
 const region = "us"
-const appIds = ["916366645"]
+const appIds = ["916366645","1067198688"]
 
 var cacheData = $persistentStore.read()
 if (!cacheData) {
