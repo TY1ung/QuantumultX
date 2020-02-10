4e225a5a75dd48fc27396c886b8b227181acf43f
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2020-02-10 21:59:39 +0800

    Update neteasemusic.cookie.js

--- Scripts/neteasemusic/neteasemusic.cookie.js
+++ Scripts/neteasemusic/neteasemusic.cookie.js
@@ -3,10 +3,10 @@ const cookieKey = 'chavy_cookie_neteasemusic'
 const cookieVal = $request.headers['Cookie']
 
 if (cookieVal) {
-  let cookie = $persistentStore.write(cookieVal, cookieKey)
+  let cookie = $prefs.setValueForKey(cookieVal, cookieKey)
   if (cookie) {
     let msg = `${cookieName}`
-    $notification.post(msg, 'Cookie写入成功', '详见日志')
+    $notify(msg, 'Cookie写入成功', '详见日志')
     console.log(msg)
     console.log(cookieVal)
   }
