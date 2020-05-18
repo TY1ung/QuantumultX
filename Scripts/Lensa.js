b593f04caa740a0b1bd9aeebfb754e2f51c2e1ca
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2020-05-18 14:42:01 +0800

    Create Lensa.js

---
+++ Scripts/Lensa.js
@@ -0,0 +1,13 @@
+/*
+Lensa人像修图解锁
+[MITM]
+hostname = subscription-service.neuralprisma.com
+[Script]
+http-response ^https?:\/\/subscription-service\.neuralprisma\.com\/subscription_get requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Xu1o/Surge/master/Script/Lensa.js,script-update-interval=0
+*/
+
+var obj = JSON.parse($response.body); 
+obj['is_valid'] = true;
+obj['is_trial'] = true;
+obj['expiration_date'] = '2970-01-01T00:00:00Z';
+$done({body: JSON.stringify(obj)});
