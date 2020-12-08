02733a84ca0b93cc01693beb2ddf5ae235c0c331
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2020-12-08 18:10:32 +0800

    Update wb_launch.js

--- Scripts/wb_launch.js
+++ Scripts/wb_launch.js
@@ -1,11 +1,5 @@
 /*
-Weibo remove start up ads
-by yichahucha
-[rewrite_local]
-^https?://m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|searchall|cardlist|page|\!/photos/pic_recommend_status) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wb_ad.js
-^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wb_launch.js
-[mitm]
-hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com
+README：https://github.com/yichahucha/surge/tree/master
  */
 
 const path1 = "/interface/sdk/sdkad.php";
