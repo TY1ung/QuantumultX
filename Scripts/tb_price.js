21391554bbd5d4c456332ee32541ef399f9a557b
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2020-12-19 23:09:24 +0800

    Update tb_price.js

--- Scripts/tb_price.js
+++ Scripts/tb_price.js
@@ -3,10 +3,10 @@ README：https://github.com/yichahucha/surge/tree/master
  */
 
 const $tool = new Tool()
-const consoleLog = false
-const url = $request.url
 const path1 = "/amdc/mobileDispatch"
 const path2 = "/gw/mtop.taobao.detail.getdetail"
+const consoleLog = false
+const url = $request.url
 
 if (url.indexOf(path1) != -1) {
     if ($tool.isResponse) {
@@ -28,19 +28,22 @@ if (url.indexOf(path1) != -1) {
         body = $base64.encode(JSON.stringify(obj))
         $done({ body })
     } else {
+        let headers = $request.headers
         let body = $request.body
-        let json = Qs2Json(body)
-        let domain = json.domain.split(" ")
-        let i = domain.length;
-        while (i--) {
-            const block = "trade-acs.m.taobao.com"
-            const element = domain[i];
-            if (element == block) {
-                domain.splice(i, 1);
+        if (headers["User-Agent"].indexOf("%E6%89%8B%E6%9C%BA%E6%B7%98%E5%AE%9D") != -1) {
+            let json = Qs2Json(body)
+            let domain = json.domain.split(" ")
+            let i = domain.length;
+            while (i--) {
+                const block = "trade-acs.m.taobao.com"
+                const element = domain[i];
+                if (element == block) {
+                    domain.splice(i, 1);
+                }
             }
+            json.domain = domain.join(" ")
+            body = Json2Qs(json)
         }
-        json.domain = domain.join(" ")
-        body = Json2Qs(json)
         $done({ body })
     }
 }
@@ -58,29 +61,36 @@ if (url.indexOf(path2) != -1) {
                 let tradeConsumerProtection = null
                 let consumerProtection = null
                 let trade = null
+                let vertical = null
                 if (value.global) {
                     tradeConsumerProtection = value.global.data.tradeConsumerProtection
                     consumerProtection = value.global.data.consumerProtection
                     trade = value.global.data.trade
+                    vertical = value.global.data.vertical
                 } else {
                     tradeConsumerProtection = value.tradeConsumerProtection
                     consumerProtection = value.consumerProtection
                     trade = value.trade
+                    vertical = value.vertical
                 }
                 if (trade && trade.useWap == "true") {
                     $done({ body })
                     sendNotify(data, shareUrl)
                 } else {
-                    if (tradeConsumerProtection) {
+                    if (vertical && vertical.hasOwnProperty("tmallhkDirectSale")) {
+                        // if (value.global) {
+                        //     value.global.data["tradeConsumerProtection"] = customTradeConsumerProtection()
+                        //     value.global.data.tradeConsumerProtection = setTradeConsumerProtection(data, value.global.data.tradeConsumerProtection)
+                        // } else {
+                        //     value["tradeConsumerProtection"] = customTradeConsumerProtection()
+                        //     value.tradeConsumerProtection = setTradeConsumerProtection(data, value.tradeConsumerProtection)
+                        // }
+                        $done({ body })
+                        sendNotify(data, shareUrl)
+                    } else if (tradeConsumerProtection) {
                         tradeConsumerProtection = setTradeConsumerProtection(data, tradeConsumerProtection)
                     } else {
-                        let vertical = value.vertical
-                        if (vertical && vertical.hasOwnProperty("tmallhkDirectSale")) {
-                            value["tradeConsumerProtection"] = customTradeConsumerProtection()
-                            value.tradeConsumerProtection = setTradeConsumerProtection(data, value.tradeConsumerProtection)
-                        } else {
-                            consumerProtection = setConsumerProtection(data, consumerProtection)
-                        }
+                        consumerProtection = setConsumerProtection(data, consumerProtection)
                     }
                     apiStack.value = JSON.stringify(value)
                     $done({ body: JSON.stringify(obj) })
@@ -160,11 +170,11 @@ function priceSummary(data) {
     listPriceDetail.pop()
     let list = listPriceDetail.concat(historySummary(data.single))
     list.forEach((item, index) => {
-        if (index == 2) {
+        if (item.Name == "双11价格") {
             item.Name = "双十一价格"
-        } else if (index == 3) {
+        } else if (item.Name == "618价格") {
             item.Name = "六一八价格"
-        } else if (index == 4) {
+        } else if (item.Name == "30天最低价") {
             item.Name = "三十天最低"
         }
         summary += `\n${item.Name}${getSpace(4)}${item.Price}${getSpace(4)}${item.Date}${getSpace(4)}${item.Difference}`
@@ -176,7 +186,7 @@ function priceSummary(data) {
 
 function historySummary(single) {
     const rexMatch = /\[.*?\]/g;
-    const rexExec = /\[(.*),(.*),"(.*)"\]/;
+    const rexExec = /\[(.*),(.*),"(.*)".*\]/;
     let currentPrice, lowest60, lowest180, lowest360
     let list = single.jiagequshiyh.match(rexMatch);
     list = list.reverse().slice(0, 360);
@@ -216,7 +226,7 @@ function historySummary(single) {
 }
 
 function difference(currentPrice, price) {
-    let difference = strip(currentPrice - price)
+    let difference = sub(currentPrice, price)
     if (difference == 0) {
         return "-"
     } else {
@@ -224,8 +234,18 @@ function difference(currentPrice, price) {
     }
 }
 
-function strip(num, precision = 12) {
-    return +parseFloat(num.toPrecision(precision));
+function sub(arg1, arg2) {
+    return add(arg1, -Number(arg2), arguments[2]);
+}
+
+function add(arg1, arg2) {
+    arg1 = arg1.toString(), arg2 = arg2.toString();
+    var arg1Arr = arg1.split("."), arg2Arr = arg2.split("."), d1 = arg1Arr.length == 2 ? arg1Arr[1] : "", d2 = arg2Arr.length == 2 ? arg2Arr[1] : "";
+    var maxLen = Math.max(d1.length, d2.length);
+    var m = Math.pow(10, maxLen);
+    var result = Number(((arg1 * m + arg2 * m) / m).toFixed(maxLen));
+    var d = arguments[2];
+    return typeof d === "number" ? Number((result).toFixed(d)) : result;
 }
 
 function requestPrice(share_url, callback) {
@@ -294,7 +314,6 @@ function customTradeConsumerProtection() {
 }
 
 function Qs2Json(url) {
-    url = url == null ? window.location.href : url;
     var search = url.substring(url.lastIndexOf("?") + 1);
     var obj = {};
     var reg = /([^?&=]+)=([^?&=]*)/g;
