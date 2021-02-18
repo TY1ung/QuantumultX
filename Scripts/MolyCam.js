30b213555368fd497584ffe0dc9d9ad6d3211b58
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2021-02-18 23:19:21 +0800

    Create MolyCam.js

---
+++ Scripts/MolyCam.js
@@ -0,0 +1,22 @@
+/*
+by saymiss
+
+[rewrite_local]
+# MolyCam and VaporCam（两个APP）
+^https:\/\/pay\.wecut\.com\/apple\/(iosVerifyReceipt|iosAppVerifyReceipt)\.php url script-response-body saymiss/MolyCam_VaporCam.js
+
+[mitm]  
+pay.wecut.com
+*/
+
+var obj = JSON.parse($response.body);
+obj=
+{
+ "msg": "处理成功",
+ "data": {
+  "isValid": 1,
+  "expiresTs": 9876543210
+ },
+ "code": 0
+};
+$done({body: JSON.stringify(obj)});
