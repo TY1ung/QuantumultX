e35a26df0aa2b69dec46a753f381c7c936050d79
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2021-06-19 00:39:13 +0800

    Update iOS_Weather_AQI_Standard.js

--- Scripts/iOS_Weather_AQI_Standard.js
+++ Scripts/iOS_Weather_AQI_Standard.js
@@ -1,20 +1,21 @@
+// 此脚本仅适用于 iOS 15 的原生天气 App
 // Developed by Hackl0us (https://github.com/hackl0us)
-// Adopted to Quantumult X Rewrite Script by laosb (https://lao.sb/gh)
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
 const aqicnToken = 'd5c94b7521f1423603d149b4c60f0970718aa88d'
 
-// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-response-body 后应该替换为添加 apicnToken 值后的脚本路径
+// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
-[rewrite_local]
-https://weather-data.apple.com/v1/weather/[\w-_]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+ url script-response-body aqicn.js
-[mitm]
-hostname = weather-data.apple.com
+	[Script]
+	iOS15美标空气质量 = type=http-response,pattern=https://weather-data.apple.com/v2/weather/[\w-]+/-?[0-9]+\.[0-9]+/-?[0-9]+\.[0-9]+\?,requires-body=true,script-path=path/to/iOS15_Weather_AQI_US.js
+
+	[MITM]
+	hostname = weather-data.apple.com
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.1',
-	US: 'EPA_NowCast.1'
+	CN: 'HJ6332012.4',
+	US: 'EPA_NowCast.4'
 }
 
 const AirQualityLevel = {
@@ -26,8 +27,8 @@ const AirQualityLevel = {
 	HAZARDOUS: 6
 }
 
-const coordRegex = /https:\/\/weather-data\.apple\.com\/v1\/weather\/[\w-]+\/([0-9]+\.[0-9]+)\/([0-9]+\.[0-9]+)\?/
-const [_, lat, lng] = $request.url.match(coordRegex)
+const coordRegex = /https:\/\/weather-data\.apple\.com\/v2\/weather\/([\w-]+)\/(-?[0-9]+\.[0-9]+)\/(-?[0-9]+\.[0-9]+)\?/
+const [_, language, lat, lng] = $request.url.match(coordRegex)
 
 function classifyAirQualityLevel(aqiIndex) {
 	if (aqiIndex >= 0 && aqiIndex <= 50) {
@@ -40,7 +41,7 @@ function classifyAirQualityLevel(aqiIndex) {
 		return AirQualityLevel.UNHEALTHY;
 	} else if (aqiIndex >= 201 && aqiIndex <= 300) {
 		return AirQualityLevel.VERY_UNHEALTHY;
-	} else if (aqiIndex >= 301 && aqiIndex <= 500) {
+	} else if (aqiIndex >= 301) {
 		return AirQualityLevel.HAZARDOUS;
 	}
 }
@@ -48,7 +49,7 @@ function classifyAirQualityLevel(aqiIndex) {
 function modifyWeatherResp(weatherRespBody, aqicnRespBody) {
 	let weatherRespJson = JSON.parse(weatherRespBody)
 	let aqicnRespJson = JSON.parse(aqicnRespBody).data
-	weatherRespJson.air_quality = constructAirQuailityNode(aqicnRespJson)
+	weatherRespJson.airQuality = constructAirQuailityNode(aqicnRespJson)
 	return JSON.stringify(weatherRespJson)
 }
 
@@ -60,6 +61,8 @@ function getPrimaryPollutant(pollutant) {
 			return 'SO2';
 		case 'no2':
 			return 'NO2';
+		case 'nox':
+			return 'NOX'
 		case 'pm25':
 			return 'PM2.5';
 		case 'pm10':
@@ -72,48 +75,56 @@ function getPrimaryPollutant(pollutant) {
 }
 
 function constructAirQuailityNode(aqicnData) {
-	let airQualityNode = { "source": "", "learnMoreURL": "", "isSignificant": true, "airQualityCategoryIndex": 1, "airQualityScale": "", "airQualityIndex": 0, "pollutants": { "CO": { "name": "CO", "amount": 0, "unit": "μg/m3" }, "SO2": { "name": "SO2", "amount": 0, "unit": "μg/m3" }, "NO2": { "name": "NO2", "amount": 0, "unit": "μg/m3" }, "PM2.5": { "name": "PM2.5", "amount": 0, "unit": "μg/m3" }, "OZONE": { "name": "OZONE", "amount": 0, "unit": "μg/m3" }, "PM10": { "name": "PM10", "amount": 0, "unit": "μg/m3" } }, "metadata": { "reported_time": 0, "longitude": 0, "provider_name": "aqicn.org", "expire_time": 2, "provider_logo": "https://i.loli.net/2020/12/27/UqW23eZLFAIbxGV.png", "read_time": 2, "latitude": 0, "v": 1, "language": "", "data_source": 0 }, "name": "AirQuality", "primaryPollutant": "" }
+	let airQualityNode = { "isSignificant": true, "learnMoreURL": "", "primaryPollutant": "", "scale": "", "categoryIndex": 0, "source": "", "pollutants": { "CO": { "name": "CO", "amount": 0, "unit": "microgramsPerM3" }, "NO": { "name": "NO", "amount": 0, "unit": "microgramsPerM3" }, "NO2": { "name": "NO2", "amount": 0, "unit": "microgramsPerM3" }, "SO2": { "name": "SO2", "amount": 0, "unit": "microgramsPerM3" }, "NOX": { "name": "NOX", "amount": 0, "unit": "microgramsPerM3" }, "OZONE": { "name": "OZONE", "amount": 0, "unit": "microgramsPerM3" }, "PM10": { "name": "PM10", "amount": 0, "unit": "microgramsPerM3" }, "PM2.5": { "name": "PM2.5", "amount": 0, "unit": "microgramsPerM3" } }, "sourceType": "station", "metadata": { "version": 2, "longitude": 0, "providerName": "aqicn.org", "providerLogo": "https://aqicn.org/mapi/logo.png", "language": "en-US", "latitude": 0, "expireTime": "", "reportedTime": "", "readTime": "", "units": "m" }, "name": "AirQuality", "index": 0 }
 	const aqicnIndex = aqicnData.aqi
+
+	airQualityNode.index = aqicnIndex
+	airQualityNode.categoryIndex = classifyAirQualityLevel(aqicnIndex)
+	airQualityNode.learnMoreURL = aqicnData.city.url + '/cn/m'
+	airQualityNode.scale = AirQualityStandard.US
 	airQualityNode.source = aqicnData.city.name
-	airQualityNode.learnMoreURL = aqicnData.city.url + '/cn'
-	airQualityNode.airQualityCategoryIndex = classifyAirQualityLevel(aqicnIndex)
-	airQualityNode.airQualityScale = AirQualityStandard.US
-	airQualityNode.airQualityIndex = aqicnIndex
-	airQualityNode.pollutants.CO.amount = aqicnData.iaqi.co?.v || -1
- 	airQualityNode.pollutants.SO2.amount = aqicnData.iaqi.so2?.v || -1
- 	airQualityNode.pollutants.NO2.amount = aqicnData.iaqi.no2?.v || -1
- 	airQualityNode.pollutants["PM2.5"].amount = aqicnData.iaqi.pm25?.v || -1
- 	airQualityNode.pollutants.OZONE.amount = aqicnData.iaqi.o3?.v || -1
- 	airQualityNode.pollutants.PM10.amount = aqicnData.iaqi.pm10?.v || -1
+	airQualityNode.primaryPollutant = getPrimaryPollutant(aqicnData.dominentpol)
+
 	airQualityNode.metadata.latitude = aqicnData.city.geo[0]
 	airQualityNode.metadata.longitude = aqicnData.city.geo[1]
-	airQualityNode.metadata.read_time = roundHours(new Date(), 'down')
-	airQualityNode.metadata.expire_time = roundHours(new Date(), 'up')
-	airQualityNode.metadata.reported_time = aqicnData.time.v
-	//airQualityNode.metadata.language = $request.headers['Accept-Language']
-	airQualityNode.primaryPollutant = getPrimaryPollutant(aqicnData.dominentpol)
+	airQualityNode.metadata.readTime = timeConversion(new Date(), 'remain')
+	airQualityNode.metadata.reportedTime = timeConversion(new Date(aqicnData.time.iso), 'remain')
+	airQualityNode.metadata.expireTime = timeConversion(new Date(aqicnData.time.iso), 'add-1h-floor')
+	airQualityNode.metadata.language = language
+
+	airQualityNode.pollutants.CO.amount = aqicnData.iaqi.co?.v || -1
+	airQualityNode.pollutants.SO2.amount = aqicnData.iaqi.so2?.v || -1
+	airQualityNode.pollutants.NO2.amount = aqicnData.iaqi.no2?.v || -1
+	airQualityNode.pollutants.NOX.amount = aqicnData.iaqi.nox?.v || -1
+	airQualityNode.pollutants["PM2.5"].amount = aqicnData.iaqi.pm25?.v || -1
+	airQualityNode.pollutants.OZONE.amount = aqicnData.iaqi.o3?.v || -1
+	airQualityNode.pollutants.PM10.amount = aqicnData.iaqi.pm10?.v || -1
+
 	return airQualityNode
 }
 
-function roundHours(time, method) {
-	switch (method) {
-		case 'up':
-			time.setHours(time.getHours() + Math.ceil(time.getMinutes() / 60));
+function timeConversion(time, action) {
+	switch (action) {
+		case 'remain':
+			time.setMilliseconds(0);
 			break;
-		case 'down':
-			time.setHours(time.getHours() + Math.floor(time.getMinutes() / 60));
+		case 'add-1h-floor':
+			time.setHours(time.getHours() + 1);
+			time.setMinutes(0, 0, 0);
 			break;
 		default:
-			console.log("Error rounding method");
+			console.log('Error time converting action.');
 	}
-	time.setMinutes(2, 0, 0);
-	return time;
+	let timeString = time.toISOString().split('.')[0] + 'Z'
+	return timeString;
 }
 
-$task.fetch({
-  url: `https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicnToken}`,
-  headers: $request.headers
-}).then((response) => {
-  let body = modifyWeatherResp($response.body, response.body)
-  $done({ body })
-}, () => $done());
+$httpClient.get(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicnToken}`, function (error, _response, data) {
+	if (error) {
+		let body = $response.body
+		$done({ body })
+	} else {
+		let body = modifyWeatherResp($response.body, data)
+		$done({ body })
+	}
+});
