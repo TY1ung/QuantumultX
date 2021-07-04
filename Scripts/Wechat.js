cd3a091caa40be6f689d40a92e376e94073bfee9
Sir.Wang <57001866+sirsunknight@users.noreply.github.com>
2021-07-05 00:52:49 +0800

    Update Wechat.js

--- Scripts/Wechat.js
+++ Scripts/Wechat.js
@@ -1,19 +1,28 @@
 /*
-Remove the WeChat public account bottom ad
+微信 去除公众号文章底部广告
 
-QX:
+***************************
+QuantumultX:
+
+[rewrite_local]
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
 
-Surge4：
+[mitm]
+hostname = mp.weixin.qq.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
 http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
 
-Surge & QX MITM = mp.weixin.qq.com,
-*/
+[MITM]
+hostname = mp.weixin.qq.com
+
+**************************/
 
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
 delete obj.appid;
 $done({body: JSON.stringify(obj)}); 
-
-//by Choler
